058672fad898248eecc2699473b00f490004c9ef
ArdianHalilaj7 <ardi.halilaj997@gmail.com>
2023-02-03 18:05:10 +0100

    Contact example

---
+++ src/Pages/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+    <div>Contact</div>
+    
+    <h1>Ardian</h1>
+    <h1>Ardian</h1>
+    <h1>Ardian</h1>
+    <h1>Ardian</h1>
+    <h1>Ardian</h1>
+    <h1>Ardian</h1>
+    <h1>Ardian</h1>
+    <h1>Ardian</h1>
+    <h1>Ardian</h1>
+    
+    
+    </>
+  )
+}
+
+export default Contact
