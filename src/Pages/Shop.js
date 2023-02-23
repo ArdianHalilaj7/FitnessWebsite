10cf5faf5978a820ad9d7b93c20ddc11099c8258
ArdianHalilaj7 <ardi.halilaj997@gmail.com>
2023-02-24 00:45:44 +0100

    Some changes

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -5,6 +5,7 @@ const styles = {
     card: {
       backgroundColor: '#000',
       borderRadius: 50,
+      padding: '1rem',
 
     },
     cardImage: {
@@ -20,10 +21,10 @@ const Shop = () => {
 
     <>
 
+
         <CardGroup className="m-5 d-block">
           <Card className="m-5 border-0 shadow" style={styles.card}>
             <Row>
-
               <Col>
                 <Card.Img src="whey.jpg" style={styles.cardImage} />
               </Col>
@@ -37,12 +38,107 @@ const Shop = () => {
                   <h6 className='h5biography'>Flavour:</h6><h4 style={{fontSize:'18px'}} className='h4biography'>Chocolate</h4>
                   <h6 className='h5biography'>Protein Information:</h6>
                   <h6 style={{fontSize:'14px'}} className='h4biography'>
-                  Our 100% Whey is the ideal post workout nutrition to speed up recovery 
-                  and build lean muscle mass. For a limited time, get a free 500g bag of 
-                  protein when you buy a 2.2kg flavour!
+                  Whether you’re a beginner just kick starting your fitness journey or 
+                  someone who’s aiming to take their workouts to the next level, Pre Load is the way to look! 
                   </h6>
                   <br/>
-                  <h5 className='h5biography'>Price:</h5><h4 style={{fontSize:'18px'}} className='h4biography'><Button><b>$129.90</b></Button></h4>
+                  <h5 className='h5biography'>Price:</h5><h4 style={{fontSize:'18px'}} className='h4biography'>
+                    <Button><b><del style={{color:'red'}}><i>$149.90</i></del> to $129.90</b></Button></h4>
+                </Card.Text>
+
+                </Card.Body>
+              </Col>
+
+            </Row>
+          </Card>
+        </CardGroup>
+
+        <CardGroup className="m-5 d-block">
+          <Card className="m-5 border-0 shadow" style={styles.card}>
+            <Row>
+
+              <Col>
+                <Card.Img src="creatine.jpg" style={styles.cardImage} />
+              </Col>
+
+              <Col>
+                <Card.Body>
+                <Card.Title style={{marginLeft:'-20px'}}><Button variant="dark" size="lg">CREATINE MONOHYDRATE</Button></Card.Title>
+                <br/>
+                <Card.Text as="h4" style={styles.cardText}>
+                  <h6 className='h5biography'>Size:</h6><h4 style={{fontSize:'18px'}} className='h4biography'>1Kg</h4>
+                  <h6 className='h5biography'>Flavour:</h6><h4 style={{fontSize:'18px'}} className='h4biography'>Monohydrate</h4>
+                  <h6 className='h5biography'>Protein Information:</h6>
+                  <h6 style={{fontSize:'14px'}} className='h4biography'>
+                  Backed by years of research, Creatine Monohydrate will help maximise 
+                  your performance gains and increase your training capacity.
+                  </h6>
+                  <br/>
+                  <h5 className='h5biography'>Price:</h5><h4 style={{fontSize:'18px'}} className='h4biography'><Button><b>$24.90</b></Button></h4>
+                </Card.Text>
+
+                </Card.Body>
+              </Col>
+
+            </Row>
+          </Card>
+        </CardGroup>
+
+        <CardGroup className="m-5 d-block">
+          <Card className="m-5 border-0 shadow" style={styles.card}>
+            <Row>
+
+              <Col>
+                <Card.Img src="preload.jpg" style={styles.cardImage} />
+              </Col>
+
+              <Col>
+                <Card.Body>
+                <Card.Title style={{marginLeft:'-20px'}}><Button variant="dark" size="lg">PRE LOAD TWIN PACK</Button></Card.Title>
+                <br/>
+                <Card.Text as="h4" style={styles.cardText}>
+                  <h6 className='h5biography'>Size:</h6><h4 style={{fontSize:'18px'}} className='h4biography'>360g + 360g</h4>
+                  <h6 className='h5biography'>Flavour:</h6><h4 style={{fontSize:'18px'}} className='h4biography'>Cirtus Clash</h4>
+                  <h6 className='h5biography'>Protein Information:</h6>
+                  <h6 style={{fontSize:'14px'}} className='h4biography'>
+                  Backed by years of research, Creatine Monohydrate will help maximise 
+                  your performance gains and increase your training capacity.
+                  </h6>
+                  <br/>
+                  <h5 className='h5biography'>Price:</h5><h4 style={{fontSize:'18px'}} className='h4biography'>
+                    <Button><b><del style={{color:'red'}}><i>$139.90</i></del> to $119.90</b></Button></h4>
+                </Card.Text>
+
+                </Card.Body>
+              </Col>
+
+            </Row>
+          </Card>
+        </CardGroup>
+
+        <CardGroup className="m-5 d-block">
+          <Card className="m-5 border-0 shadow" style={styles.card}>
+            <Row>
+
+              <Col>
+                <Card.Img src="isozero.jpg" style={styles.cardImage} />
+              </Col>
+
+              <Col>
+                <Card.Body>
+                <Card.Title style={{marginLeft:'-20px'}}><Button variant="dark" size="lg">ISO ZERO</Button></Card.Title>
+                <br/>
+                <Card.Text as="h4" style={styles.cardText}>
+                  <h6 className='h5biography'>Size:</h6><h4 style={{fontSize:'18px'}} className='h4biography'>750g</h4>
+                  <h6 className='h5biography'>Flavour:</h6><h4 style={{fontSize:'18px'}} className='h4biography'>Chocolate</h4>
+                  <h6 className='h5biography'>Protein Information:</h6>
+                  <h6 style={{fontSize:'14px'}} className='h4biography'>
+                  If you are looking to maximise your protein intake whilst reducing other macros 
+                  like fats and carbs - Emrald Labs Iso Zero is ideal for you!
+                  </h6>
+                  <br/>
+                  <h5 className='h5biography'>Price:</h5><h4 style={{fontSize:'18px'}} className='h4biography'>
+                    <Button><b>$84.90</b></Button></h4>
                 </Card.Text>
 
                 </Card.Body>
@@ -53,7 +149,6 @@ const Shop = () => {
         </CardGroup>
 
         
-
 
 
     </>
